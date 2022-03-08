031431a4898212bf2fea30da56b65dfab957c6f1
yumegan <yu.megan@gmail.com>
2022-03-08 14:14:25 -0500

    continued Lab06, around 21:25 in vid

--- app/src/main/java/com/example/p3l03ayumegan4corners/Lab06.java
+++ app/src/main/java/com/example/p3l03ayumegan4corners/Lab06.java
@@ -1,14 +1,48 @@
 package com.example.p3l03ayumegan4corners;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.viewpager2.adapter.FragmentStateAdapter;
+import androidx.viewpager2.widget.ViewPager2;
 
 import android.os.Bundle;
 
 public class Lab06 extends AppCompatActivity {
+    ViewPager2 mViewPager2;
+    RecyclerView.Adapter mMyFragmentStateAdapter;
+    int NUM_ITEMS = 6;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lab06);
+        // assign the instance of ViewPager2
+        mViewPager2 = findViewById(R.id.container);
+        // create an adapter for ViewPager2
+        mMyFragmentStateAdapter = new MyFragmentStateAdapter(this);
+        // set the adapter for the ViewPager
+        mViewPager2.setAdapter(mMyFragmentStateAdapter);
+        // gets to the end but error???
+    }
+
+    private class MyFragmentStateAdapter extends FragmentStateAdapter {
+
+        public MyFragmentStateAdapter(@NonNull FragmentActivity fragmentActivity) {
+            super(fragmentActivity);
+        }
+
+        @NonNull
+        @Override
+        public Fragment createFragment(int position) {
+            return MainFragment.newInstance();
+        }
+
+        @Override
+        public int getItemCount() {
+            return NUM_ITEMS;
+        }
     }
 }
---
+++ app/src/main/java/com/example/p3l03ayumegan4corners/MainFragment.java
@@ -0,0 +1,10 @@
+package com.example.p3l03ayumegan4corners;
+
+import androidx.fragment.app.Fragment;
+
+public class MainFragment extends Fragment {
+
+    public static Fragment newInstance(ViewPager2 mViewPager2) {
+
+    }
+}
