54208fe77f470935ca1aa2f1de12050e9f44673d
yumegan <yu.megan@gmail.com>
2022-03-14 22:26:49 -0400

    Lab06b added zoom out transition. sound effect only works sometimes. why.

--- app/src/main/java/com/example/p3l03ayumegan4corners/MainFragment.java
+++ app/src/main/java/com/example/p3l03ayumegan4corners/MainFragment.java
@@ -1,6 +1,9 @@
 package com.example.p3l03ayumegan4corners;
 
+import android.content.Context;
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -21,8 +24,11 @@ public class MainFragment extends Fragment {
     Button mButton;
     int position;
 
+    MediaPlayer mp; // unused?
+
     public static Fragment newInstance(ViewPager2 mViewPager2, int position) {
         MainFragment fragment = new MainFragment();
+        mViewPager2.setPageTransformer(new ZoomOutPageTransformer());
         fragment.mViewPager2 = mViewPager2;
         fragment.position = position;
         return fragment;
@@ -31,12 +37,17 @@ public class MainFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
         return inflater.inflate(R.layout.fragment_main, container, false);
     }
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+
+        MediaPlayer song = MediaPlayer.create(getActivity(), R.raw.sneeze);
+        song.start();
+
 
         // add to tablayout
         TabLayout tabLayout = getActivity().findViewById(R.id.tab_layout);
@@ -52,8 +63,55 @@ public class MainFragment extends Fragment {
         mButton.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v){
-                Toast.makeText(getContext(), "toasty"+position, Toast.LENGTH_LONG).show();
+                Toast.makeText(getContext(), "toasty #"+position+"!", Toast.LENGTH_LONG).show();
             }
         });
+    }
+
+
+
+//    public void noise(Context context){
+//
+//    }
+}
+
+class ZoomOutPageTransformer implements ViewPager2.PageTransformer {
+    private static final float MIN_SCALE = 0.85f;
+    private static final float MIN_ALPHA = 0.5f;
+
+    public void transformPage(View view, float position) {
+
+
+        int pageWidth = view.getWidth();
+        int pageHeight = view.getHeight();
+
+        if (position < -1) { // [-Infinity,-1)
+            // This page is way off-screen to the left.
+            view.setAlpha(0f);
+
+        } else if (position <= 1) { // [-1,1]
+            // Modify the default slide transition to shrink the page as well
+            float scaleFactor = Math.max(MIN_SCALE, 1 - Math.abs(position));
+            float vertMargin = pageHeight * (1 - scaleFactor) / 2;
+            float horzMargin = pageWidth * (1 - scaleFactor) / 2;
+            if (position < 0) {
+                view.setTranslationX(horzMargin - vertMargin / 2);
+            } else {
+                view.setTranslationX(-horzMargin + vertMargin / 2);
+            }
+
+            // Scale the page down (between MIN_SCALE and 1)
+            view.setScaleX(scaleFactor);
+            view.setScaleY(scaleFactor);
+
+            // Fade the page relative to its size.
+            view.setAlpha(MIN_ALPHA +
+                    (scaleFactor - MIN_SCALE) /
+                            (1 - MIN_SCALE) * (1 - MIN_ALPHA));
+
+        } else { // (1,+Infinity]
+            // This page is way off-screen to the right.
+            view.setAlpha(0f);
+        }
     }
 }
