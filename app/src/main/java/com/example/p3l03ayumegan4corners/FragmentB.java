4c775c3269b30c04cdafda010b4e9ddc7d2d71a1
yumegan <yu.megan@gmail.com>
2022-03-17 14:11:46 -0400

    started lab07 - 34:37 in vid

---
+++ app/src/main/java/com/example/p3l03ayumegan4corners/FragmentB.java
@@ -0,0 +1,41 @@
+package com.example.p3l03ayumegan4corners;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class FragmentB extends Fragment {
+    View view;
+    public static Fragment newInstance(int x, String s){
+        FragmentB fragmentB = new FragmentB();
+        Bundle args = new Bundle();
+        args.putInt("MyAge", x);
+        args.putString("MyName", s);
+        fragmentB.setArguments(args);
+        return fragmentB;
+    }
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_b, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        this.view = view;
+        int age = getArguments().getInt("MyAge", 10000000);
+        String name = getArguments().getString("MyName", "Megan Thee Stallion");
+        TextView textview = view.findViewById(R.id.b_textview);
+        textview.setText(R.string.greeting_lab07);
+        Button button = view.findViewById(R.id.b_button);
+        button.setText(R.string.click_me);
+    }
+}
--- app/src/main/java/com/example/p3l03ayumegan4corners/Lab07.java
+++ app/src/main/java/com/example/p3l03ayumegan4corners/Lab07.java
@@ -1,8 +1,11 @@
 package com.example.p3l03ayumegan4corners;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class Lab07 extends AppCompatActivity {
 
@@ -10,5 +13,16 @@ public class Lab07 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lab07);
+        // begin transaction
+        FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+        // add contents to container
+        ft.add(R.id.fragment_container, FragmentB.newInstance(17, "Megan Thee Yu"), "FragmentB");
+        // complete the changes added above
+        ft.commit();
+    }
+
+    public void update_greeting(View view) {
+        FragmentB fragmentB = (FragmentB)getSupportFragmentManager().findFragmentByTag("FragmentB");
+        ((TextView)(fragmentB.view.findViewById(R.id.b_textview))).setText(R.string.greeting2_lab07);
     }
 }
