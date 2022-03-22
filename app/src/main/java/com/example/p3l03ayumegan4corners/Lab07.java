d0970adc3171eb8ae891c0db260f99cd32ad0882
yumegan <yu.megan@gmail.com>
2022-03-21 22:56:14 -0400

    finished lab07b - changes between fragments, added pic of me

--- app/src/main/java/com/example/p3l03ayumegan4corners/Lab07.java
+++ app/src/main/java/com/example/p3l03ayumegan4corners/Lab07.java
@@ -8,11 +8,15 @@ import android.view.View;
 import android.widget.TextView;
 
 public class Lab07 extends AppCompatActivity {
+    TextView change_frag;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_lab07);
+
+        change_frag = findViewById(R.id.change_frag);
         // begin transaction
         FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
         // add contents to container
@@ -22,7 +26,23 @@ public class Lab07 extends AppCompatActivity {
     }
 
     public void update_greeting(View view) {
+//        System.out.println("change_frag " + change_frag);
+        String cur_msg = (String) change_frag.getText();
+        System.out.println(cur_msg + getString(R.string.next_frag));
+
         FragmentB fragmentB = (FragmentB)getSupportFragmentManager().findFragmentByTag("FragmentB");
-        ((TextView)(fragmentB.view.findViewById(R.id.b_textview))).setText(R.string.greeting2_lab07);
+
+        if (cur_msg.equals(getString(R.string.next_frag))){
+            ((TextView)(fragmentB.view.findViewById(R.id.b_textview))).setText(R.string.greeting2_lab07);
+
+            change_frag.setText(R.string.hide_msg);
+        }
+        else{
+            ((TextView)(fragmentB.view.findViewById(R.id.b_textview))).setText(getString(R.string.greeting_lab07, "Megan Thee Yu"));
+            change_frag.setText(R.string.next_frag);
+
+        }
+
+
     }
 }
